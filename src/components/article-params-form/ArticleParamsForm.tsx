7faff89dc52f20e57696c26b354d709e434a74e0
Maria Glazkova <marusya86_msk@yandex.ru>
2025-11-19 00:58:13 +0300

    feat add sidebar logic

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,144 @@
+import { FormEvent } from 'react';
+import clsx from 'clsx';
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import { Text } from 'src/ui/text';
+import {
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	type ArticleStateType,
+	type OptionType,
+} from 'src/constants/articleProps';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+const FORM_ID = 'article-params-form';
+
+type ArticleParamsFormProps = {
+	formState: ArticleStateType;
+	isOpen: boolean;
+	onChange: (field: keyof ArticleStateType, value: OptionType) => void;
+	onFormReset: () => void;
+	onApply: () => void;
+	onToggle: () => void;
+	onClose: () => void;
+};
+
+export const ArticleParamsForm = ({
+	formState,
+	isOpen,
+	onChange,
+	onFormReset,
+	onApply,
+	onToggle,
+	onClose,
+}: ArticleParamsFormProps) => {
+	const handleOptionChange =
+		(field: keyof ArticleStateType) => (option: OptionType) => {
+			onChange(field, option);
+		};
+
+	const handleFontSizeChange = (option: OptionType) => {
+		onChange('fontSizeOption', option);
+	};
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		onApply();
+	};
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton isOpen={isOpen} onClick={onToggle} ariaControls={FORM_ID} />
+			<div
+				className={clsx(styles.overlay, {
+					[styles.overlay_visible]: isOpen,
+				})}
+				onClick={onClose}
+				role='presentation'
+			/>
+			<aside
+				id={FORM_ID}
+				className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<h2 className={styles.title}>
+						<Text as='span' size={31} weight={800}>
+							ЗАДАЙТЕ ПАРАМЕТРЫ
+						</Text>
+					</h2>
+
+					<div className={styles.field}>
+						<Select
+							title='ШРИФТ'
+							selected={formState.fontFamilyOption}
+							options={fontFamilyOptions}
+							onChange={handleOptionChange('fontFamilyOption')}
+						/>
+					</div>
+
+					<div className={styles.field}>
+						<div className={styles.label}>
+							<Text size={12} weight={800} uppercase>
+								РАЗМЕР ШРИФТА
+							</Text>
+						</div>
+						<div className={styles.fontSizeButtons}>
+							{fontSizeOptions.map((option) => (
+								<button
+									key={option.value}
+									type='button'
+									className={clsx(styles.fontSizeButton, {
+										[styles.fontSizeButton_active]:
+											formState.fontSizeOption.value === option.value,
+									})}
+									onClick={() => handleFontSizeChange(option)}>
+									<Text size={18} weight={800}>
+										{option.title.toUpperCase()}
+									</Text>
+								</button>
+							))}
+						</div>
+					</div>
+
+					<div className={styles.field}>
+						<Select
+							title='ЦВЕТ ШРИФТА'
+							selected={formState.fontColor}
+							options={fontColors}
+							onChange={handleOptionChange('fontColor')}
+						/>
+					</div>
+
+					<div className={styles.field}>
+						<Select
+							title='ЦВЕТ ФОНА'
+							selected={formState.backgroundColor}
+							options={backgroundColors}
+							onChange={handleOptionChange('backgroundColor')}
+						/>
+					</div>
+
+					<div className={styles.field}>
+						<Select
+							title='ШИРИНА КОНТЕНТА'
+							selected={formState.contentWidth}
+							options={contentWidthArr}
+							onChange={handleOptionChange('contentWidth')}
+						/>
+					</div>
+
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
+						<Button
+							title='СБРОСИТЬ'
+							htmlType='button'
+							type='clear'
+							onClick={onFormReset}
+						/>
+						<Button title='ПРИМЕНИТЬ' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,21 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { StrictMode, CSSProperties, useEffect, useState } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	defaultArticleState,
+	type ArticleStateType,
+	type OptionType,
+} from './constants/articleProps';
+import {
+	loadFormState,
+	loadPageState,
+	saveFormState,
+	savePageState,
+	normalizeArticleState,
+} from './utils/articleStateStorage';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
@@ -13,19 +24,70 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [articleState, setArticleState] = useState<ArticleStateType>(() =>
+		loadPageState()
+	);
+	const [formState, setFormState] = useState<ArticleStateType>(() =>
+		loadFormState()
+	);
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	useEffect(() => {
+		savePageState(articleState);
+	}, [articleState]);
+
+	useEffect(() => {
+		saveFormState(formState);
+	}, [formState]);
+
+	const handleFormChange = (
+		field: keyof ArticleStateType,
+		value: OptionType
+	) => {
+		setFormState((prevState) => ({
+			...prevState,
+			[field]: value,
+		}));
+	};
+
+	const handleFormReset = () => {
+		const resetState = normalizeArticleState(defaultArticleState);
+
+		setFormState(resetState);
+		setArticleState(resetState);
+	};
+
+	const handleApply = () => {
+		setArticleState(formState);
+	};
+
+	const handleSidebarToggle = () => {
+		setIsSidebarOpen((prevState) => !prevState);
+	};
+
+	const handleSidebarClose = () => {
+		setIsSidebarOpen(false);
+	};
+
+	const mainStyle = {
+		'--font-family': articleState.fontFamilyOption.value,
+		'--font-size': articleState.fontSizeOption.value,
+		'--font-color': articleState.fontColor.value,
+		'--container-width': articleState.contentWidth.value,
+		'--bg-color': articleState.backgroundColor.value,
+	} as CSSProperties;
+
 	return (
-		<main
-			className={clsx(styles.main)}
-			style={
-				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
-				} as CSSProperties
-			}>
-			<ArticleParamsForm />
+		<main className={clsx(styles.main)} style={mainStyle}>
+			<ArticleParamsForm
+				formState={formState}
+				isOpen={isSidebarOpen}
+				onChange={handleFormChange}
+				onFormReset={handleFormReset}
+				onApply={handleApply}
+				onToggle={handleSidebarToggle}
+				onClose={handleSidebarClose}
+			/>
 			<Article />
 		</main>
 	);
